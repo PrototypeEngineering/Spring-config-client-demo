b72d29818259d1aa5394ac103976e899aec56c68
Midhun <midhun.connect@gmail.com>
2020-09-01 16:33:41 +0530

    My First commit

---
+++ SpringBootConfigClient/src/main/java/com/config/client/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class ConfigClientController {
+	
+	@Value("${my.greeting: default value}")
+	private String message;
+	
+		
+	public ConfigClientController() {
+		
+	}
+	
+	@GetMapping("/client")
+	public String getConfig() {
+		return "my.greeting:"+ message;
+	}
+	
+	@GetMapping("/")
+	public String isActive() {
+		return "Running";
+	}
+	
+
+}
